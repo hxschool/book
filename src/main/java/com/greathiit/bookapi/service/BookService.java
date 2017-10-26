e89fa0e62f370c1458980757a5d949bf158206b0
zhaojunfei <773152@qq.com>
2017-10-26 18:17:23 +0800

    增加日志记录

--- src/main/java/com/greathiit/bookapi/service/BookService.java
+++ src/main/java/com/greathiit/bookapi/service/BookService.java
@@ -3,35 +3,48 @@ package com.greathiit.bookapi.service;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.greathiit.bookapi.web.BookController;
+
 @Service
 @Transactional(readOnly=true)
 public class BookService {
+	private final static Logger log = LoggerFactory.getLogger(BookController.class);
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
 	public List<Map<String,Object>> getBook(String name){
 		String sql = "select * from 馆藏书目库  where 题名 is not null and 题名 like ?";
-		return jdbcTemplate.queryForList(sql, "%"+name+"%");
+		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, "%"+name+"%");
+		log.info("打印查询sql :{} ,参数  :{}",sql,name);
+		return list;
 	}
 	
 	public List<Map<String,Object>> getSortBook(String name){
 		String sql = "select * from 馆藏书目库  where 题名 is not null and 题名缩写  like ?";
-		return jdbcTemplate.queryForList(sql, "%"+name+"%");
+		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, "%"+name+"%");
+		log.info("打印查询sql :{} ,参数  :{}",sql,name);
+		return list;
 	}
 	
 	public List<Map<String,Object>> getReader(String name){
 		String sql = "select * from 读者库  where 姓名 is not null and 姓名  like ?";
-		return jdbcTemplate.queryForList(sql, "%"+name+"%");
+		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, "%"+name+"%");
+		log.info("打印查询sql :{} ,参数  :{}",sql,name);
+		return list;
 	}
 	
 	public List<Map<String,Object>> getReaderByStudentNumber(String studentNumber){
 		String sql = "select * from 读者库  where 姓名 is not null and 读者条码  like ?";
-		return jdbcTemplate.queryForList(sql, "%"+studentNumber+"%");
+		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, "%"+studentNumber+"%");
+		log.info("打印查询sql :{} ,参数  :{}",sql,studentNumber);
+		return list;
 	}
 	
 	
--- src/main/java/com/greathiit/bookapi/web/BookController.java
+++ src/main/java/com/greathiit/bookapi/web/BookController.java
@@ -3,6 +3,8 @@ package com.greathiit.bookapi.web;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,31 +13,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.greathiit.bookapi.service.BookService;
 
 @Controller
-@RequestMapping(value = "/book")
+@RequestMapping(value = "/api")
 public class BookController {
+	private final static Logger log = LoggerFactory.getLogger(BookController.class);
 	@Autowired
 	private BookService bookService;
 	@RequestMapping(value = "/getBook")
 	@ResponseBody
 	public List<Map<String,Object>> getBook(String bookname){
+		log.info("查询书名,请求参数:{}",bookname);
 		return bookService.getBook(bookname);
 	}
 	
 	@RequestMapping(value = "/getSortBook")
 	@ResponseBody
 	public List<Map<String,Object>> getSortBook(String sortBookname){
+		log.info("查询书名缩写,请求参数:{}",sortBookname);
 		return bookService.getSortBook(sortBookname);
 	}
 	
 	@RequestMapping(value = "/getReader")
 	@ResponseBody
 	public List<Map<String,Object>> getReader(String name){
+		log.info("查询读者信息,请求参数:{}",name);
 		return bookService.getReader(name);
 	}
 	
 	@RequestMapping(value = "/getReaderByStudentNumber")
 	@ResponseBody
 	public List<Map<String,Object>> getReaderByStudentNumber(String studentNumber){
+		log.info("查询读者信息,通过学号查询请求参数:{}",studentNumber);
 		return bookService.getReaderByStudentNumber(studentNumber);
 	}
 	
