1699a68d50034f726984dffcd19db1cbdb4a53c0
zhaojunfei <773152@qq.com>
2017-11-03 12:43:22 +0800

    只查询前100行数据

--- src/main/java/com/greathiit/bookapi/service/BookService.java
+++ src/main/java/com/greathiit/bookapi/service/BookService.java
@@ -20,28 +20,28 @@ public class BookService {
 	private JdbcTemplate jdbcTemplate;
 	
 	public List<Map<String,Object>> getBook(String name){
-		String sql = "select * from 馆藏书目库  where 题名 is not null and charindex(?,题名)>0";
+		String sql = "select top 100 * from 馆藏书目库  where 题名 is not null and charindex(?,题名)>0";
 		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, name);
 		log.info("打印查询sql :{} ,参数  :{}",sql,name);
 		return list;
 	}
 	
 	public List<Map<String,Object>> getSortBook(String sortBookname){
-		String sql = "select * from 馆藏书目库  where 题名 is not null and charindex(?,题名缩写)>0";
+		String sql = "select top 100 * from 馆藏书目库  where 题名 is not null and charindex(?,题名缩写)>0";
 		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql, sortBookname);
 		log.info("打印查询sql :{} ,参数  :{}",sql,sortBookname);
 		return list;
 	}
 	
 	public List<Map<String,Object>> getReader(String name){
-		String sql = "select * from 读者库  where 姓名 is not null and  charindex(?,姓名)>0";
+		String sql = "select top 100 * from 读者库  where 姓名 is not null and  charindex(?,姓名)>0";
 		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,name);
 		log.info("打印查询sql :{} ,参数  :{}",sql,name);
 		return list;
 	}
 	
 	public List<Map<String,Object>> getReaderByStudentNumber(String studentNumber){
-		String sql = "select * from 读者库  where 姓名 is not null and charindex(?,读者条码)>0";
+		String sql = "select top 100 * from 读者库  where 姓名 is not null and charindex(?,读者条码)>0";
 		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,studentNumber);
 		log.info("打印查询sql :{} ,参数  :{}",sql,studentNumber);
 		return list;
